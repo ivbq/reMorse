d753d70d20cc834546efb77bab4a0f39b86dc71f
Ivar <ivarbo@kth.se>
2022-11-14 19:22:01 +0100

    fixa mitt misstag

--- interpreter.py
+++ interpreter.py
@@ -16,25 +16,19 @@
 #6 110 jmp                    3bit op + 1bit (+/-)     + 4bit (16)             8bit
 #7 111 jme                    3bit op + 1bit (imm/var) + 2bit var  + 2bit      8bit
 
-
-
 register = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
 regBin = ['a', 'b', 'c', 'd']
 opBin = ["inp", "out", "end", "add/sub", "addi", "subi", "jmp", "jme"]
 
 #morse to hex to bin
-morseDic = {"-----":'0', ".----":'1', "..---":'2', "...--":'3', "....-":'4', ".....":'5', "-....":'6', "--...":'7', "---..":'8', "----.":'9', ".-":'A', "-...":'B', "-.-.":'C', "-..":'D', ".":'E', "..-.":'F'}
+morseDic = {"-----": '0', ".----": '1', "..---": '2', "...--": '3', "....-": '4', ".....": '5', "-....": '6', "--...": '7', "---..": '8', "----.": '9', ".-": 'A', "-...": 'B', "-.-.": 'C', "-..": 'D', ".": 'E', "..-.": 'F'}
 
-def morseToBinary(a:str):
+def morseToBinary(a: str):
+    """Converts from morse code in text to a binary string"""
     x, y = a.split()
     binX = format(int(morseDic[x], 16), '0>4b')
     binY = format(int(morseDic[y], 16), '0>4b')
-    return (binX+binY)
-
-
-
-
-
+    return (binX + binY)
 
 def evaluate(command: str, debug: bool = False):
     """Evaluates statement, updates variables, and returns relative index of next statement to evaluate"""
@@ -44,60 +38,71 @@ def evaluate(command: str, debug: bool = False):
     
     match op:
         case "inp": #måste vara int
-            x = regBin[int(binary[3:5],2)]
+            x = regBin[int(binary[3:5], 2)]
             if debug: print(f"inp -> {x} = ",end="")
-            register[x] = input()
+            register[x] = int(input())
             return 1
 
         case "out":
-            x = regBin[int(binary[3:5],2)]
-            if debug: print(f"out -> ",end="")
+            x = regBin[int(binary[3:5], 2)]
+            if debug: print(f"out {x} -> ",end="")
             print(register[x])
             return 1
 
         case "end": #good enough
-            return 100
+            return 1000000
         
         case "add/sub":
-            x = regBin[int(binary[4:6],2)]
-            y = regBin[int(binary[6:8],2)]
+            x = register[regBin[int(binary[4:6], 2)]]
+            y = register[regBin[int(binary[6:], 2)]]
 
-            if binary[3] == 1: register[x] = int(register[x]) - int(register[y])
-            else: register[x] = int(register[x]) + int(register[y])
+            if binary[3] == '1': 
+                if debug: print(f"sub {x} - {y}")
+                x += y
+            else: 
+                if debug: print(f"add {x} + {y}")
+                x += y
             return 1
         
         case "addi":
-            x = regBin[int(binary[3:5],2)]
-            y = int(binary[5:],2)
+            x = regBin[int(binary[3:5], 2)]
+            y = int(binary[5:], 2)
             register[x] = int(register[x]) + y
+            if debug: print(f"addi {x} + {y}")
             return 1
 
         case "subi":
-            x = regBin[int(binary[3:5],2)]
+            x = regBin[int(binary[3:5], 2)]
             y = int(binary[5:],2)
             register[x] = int(register[x]) - y
+            if debug: print(f"subbi {x} - {y}")
             return 1
 
         case "jmp": #check så att i inte blir negativt
-            x = int(binary[4:],2)
-            if binary[3] == 1: return -x
-            else: return x
+            x = int(binary[4:], 2)
+            if binary[3] == '1': x = -x
+            if debug: print(f"jmp {x}")
+            return x
 
         case "jme":
-            x = regBin[int(binary[4:6],2)]
-            y = int(binary[6:8],2)
+            x = register[regBin[int(binary[4:6],2)]]
+            y = int(binary[6:], 2)
 
-            if binary[3] == 1: y = register[regBin[y]] #variabel
-
-            if x == y: return 2
-            else: return 1
+            if binary[3] == '1': y = register[regBin[y]] #variabel
+            if int(x) == int(y):
+                if debug: print(f"equal: {x} och {y}")
+                input()
+                return 2
+            else: 
+                if debug: print(f"not equal: {x} och {y}")
+                input()
+                return 1
 
             #xv, yv = int(register[x]), int(register[y]) if y in register else int(y)
             #if debug: print(f"jme {x} {y} -> {xv == yv}")
             #if xv == yv: return 2
             #else: return 1
-        
-            
+
         case _:
             if debug: print(f"Line not evaluated")
             return 1
---
+++ kodTillMorse.py
@@ -0,0 +1,38 @@
+binToMorse = {'0000': "-----", '0001': ".----", '0010': "..---", '0011': "...--", '0100': "....-", '0101': ".....", '0110': "-....", '0111': "--...", '1000': "---..", '1001': "----.", '1010': ".-", '1011': "-...", '1100': "-.-.", '1101': "-..", '1110': ".", '1111': "..-."}
+varToBin = {'a': "00", 'b': "01", 'c': "10", 'd': "11"}
+
+def toMorse(command):
+    match command.split():
+        case ["inp", x]:
+            binary = "000" + varToBin[x] + "000"
+        case ["out", x]:
+            binary = "001" + varToBin[x] + "000"
+        case ["end"]:
+            binary = "01000000"
+        case ["add", x, y]:
+            binary = "0110" + varToBin[x] + varToBin[y]
+        case ["sub", x, y]:
+            binary = "0111" + varToBin[x] + varToBin[y]
+        case ["addi", x, y]:
+            binary = "100" + varToBin[x] + format(int(y), '0>3b')
+        case ["subi", x, y]:
+            binary = "101" + varToBin[x] + format(int(y), '0>3b')
+        case ["jmp", x]:
+            binary = "110"
+            if int(x) < 0: binary += '1' + format(-int(x), '0>4b')
+            else: binary += '0' + format(int(x), '0>4b')
+        case ["jme", x, y]: #help
+            binary = "111"
+            if y in varToBin: binary += '1' + varToBin(x) + varToBin(y)
+            else: binary += '0' + varToBin(x) + format(int(y), '0>2b')
+        case _:
+            binary = "00000000"
+    print(binary[:4] + ' ' + binary[4:])
+    morse = binToMorse[binary[:4]] + ' ' + binToMorse[binary[4:]]
+    print(morse)
+
+fn = input("File: ")
+with open(fn, "r") as source:
+    commands: list[str] = source.readlines()
+    for line in commands:
+        toMorse(line)
